4df1fb4c1caadde39667f7b9e95e1adc87b7155d
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2015-07-24 14:04:28 +0200

    Update Fractal - DSL for component creation

--- client/App.fsx
+++ client/App.fsx
@@ -15,9 +15,9 @@ open Model
 module App =
     type TodoItemState = {editingText : string}
     type TodoItemProps = {todo : Todo; editing : bool}
-    type todoItem =  FractalComponent<TodoItemProps, TodoItemState>
+    type todoItem = FractalComponent<TodoItemProps, TodoItemState>
 
-    let TodoItem props =
+    module TodoItemHandlers =
         let onToggle (c : todoItem) (e : FormEvent) =
             Message.publish "todo.toggle" c.props.todo.id
 
@@ -47,36 +47,32 @@ module App =
         let handleChange (c : todoItem) (e : FormEvent) =
             c.setState {editingText = e.target.value }
 
-        let getInitialState (c : todoItem) =
-            {editingText = c.props.todo.title }
+    let TodoItem =
+        Fractal.createComponent [|
+            GetInitialState (fun (c : todoItem) -> {editingText = c.props.todo.title })
 
-        let shouldComponentUpdate nextProps nextState (c : todoItem)=
-            nextProps.todo <> c.props.todo ||
-            nextProps.editing <> c.props.editing ||
-            nextState.editingText <> c.state.editingText
+            ShouldComponentUpdate ( fun nextProps nextState (c : todoItem) ->
+                nextProps.todo <> c.props.todo ||
+                nextProps.editing <> c.props.editing ||
+                nextState.editingText <> c.state.editingText)
 
-        let componentDidUpdate prevProps (prevState : obj) (c : todoItem)  =
-            if prevProps.editing |> not && c.props.editing then
-                let node = Globals.findDOMNode(c.refs.["editField"]) |> unbox<JQuery>
-                node.focus() |> ignore
+            ComponentDidUpdate (fun prevProps prevState (c : todoItem) ->
+                if prevProps.editing |> not && c.props.editing then
+                    let node = Globals.findDOMNode(c.refs.["editField"]) |> unbox<JQuery>
+                    node.focus() |> ignore)
 
-        let render (c : todoItem) =
-            DOM.li ((if c.props.todo.completed then [|ClassName "completed"|]
-                     elif c.props.editing then [|ClassName "editing"|]
-                     else [| |]),
-                DOM.div ([|ClassName "view"|],
-                    DOM.input( [| ClassName "toggle"; Attr.Type "checkbox"; Checked c.props.todo.completed; OnChange (onToggle c)|]),
-                    DOM.label([| OnDoubleClick (handleEdit c) |], c.props.todo.title),
-                    DOM.button([| ClassName "destroy"; OnClick (onDestroy c) |])),
-                DOM.input([| Ref "editField"; ClassName "edit"; Value c.state.editingText;
-                             OnBlur (handleSubmit c); OnChange ( handleChange c); OnKeyDown (handleKeyDown c) |])
-            )
-        Fractal.defineComponent render
-        |> Fractal.getInitialState getInitialState
-        |> Fractal.shouldComponentUpdate shouldComponentUpdate
-        |> Fractal.componentDidUpdate componentDidUpdate
-        |> Fractal.createComponent
-        |> Fractal.createElement props
+            Render ( fun (c : todoItem) ->
+                DOM.li ((if c.props.todo.completed then [|ClassName "completed"|]
+                         elif c.props.editing then [|ClassName "editing"|]
+                         else [| |]),
+                    DOM.div ([|ClassName "view"|],
+                        DOM.input( [| ClassName "toggle"; Attr.Type "checkbox"; Checked c.props.todo.completed; OnChange (TodoItemHandlers.onToggle c)|]),
+                        DOM.label([| OnDoubleClick (TodoItemHandlers.handleEdit c) |], c.props.todo.title),
+                        DOM.button([| ClassName "destroy"; OnClick (TodoItemHandlers.onDestroy c) |])),
+                    DOM.input([| Ref "editField"; ClassName "edit"; Value c.state.editingText;
+                                 OnBlur (TodoItemHandlers.handleSubmit c); OnChange ( TodoItemHandlers.handleChange c); OnKeyDown (TodoItemHandlers.handleKeyDown c) |])
+                ))
+         |]
 
     type FilterTodo =
         | All
@@ -88,7 +84,7 @@ module App =
         canRedo : bool; nowShowing : FilterTodo}
     type todoFooter = FractalComponent<TodoFooterProps, Nothing>
 
-    let TodoFooter props =
+    module TodoFooterHandlers =
         let onClearCompleted (e : React.MouseEvent) =
             Message.publish "todo.repository.clearSelected" ()
 
@@ -98,62 +94,42 @@ module App =
         let onRedo (e : React.MouseEvent) =
             Message.publish "todo.repository.redo" ()
 
-        let render (c : todoFooter) =
-            let clearButton =
-                DOM.button([| ClassName "clear-completed"; OnClick (onClearCompleted); Disabled (c.props.completeCount > 0) |], "Clear completed")
+    let TodoFooter =
+        Fractal.createComponent [|
+            Render (fun (c : todoFooter) ->
+                let clearButton =
+                    DOM.button([| ClassName "clear-completed"; OnClick (TodoFooterHandlers.onClearCompleted); Disabled (c.props.completeCount > 0) |], "Clear completed")
 
-            let undoButton =
-                DOM.button([| ClassName "clear-completed"; OnClick (onUndo); Disabled (c.props.canUndo) |], "Undo")
+                let undoButton =
+                    DOM.button([| ClassName "clear-completed"; OnClick (TodoFooterHandlers.onUndo); Disabled (c.props.canUndo) |], "Undo")
 
-            let redoButton =
-                DOM.button([| ClassName "clear-completed"; OnClick (onRedo); Disabled (c.props.canRedo) |], "Redo") 
+                let redoButton =
+                    DOM.button([| ClassName "clear-completed"; OnClick (TodoFooterHandlers.onRedo); Disabled (c.props.canRedo) |], "Redo")
 
-            DOM.footer( [| ClassName "footer" |],
-                DOM.span( [| ClassName "todo-count" |],
-                    DOM.strong([| |], c.props.count),
-                    " todo(s) left"
-                ),
-                DOM.ul( [| ClassName "filters" |],
-                    DOM.li( [||],
-                        DOM.a( [| Href "#/"; ClassName (if c.props.nowShowing = FilterTodo.All then "selected" else "" ) |]  , "All"),
-                        " ",
-                        DOM.a( [| Href "#/active"; ClassName (if c.props.nowShowing = FilterTodo.Active then "selected" else "" ) |]  , "Active"),
-                        " ",
-                        DOM.a( [|Href "#/completed"; ClassName (if c.props.nowShowing = FilterTodo.Completed then "selected" else "" ) |]  , "Completed")
-                    )
-                ),
-                clearButton,
-                undoButton,
-                redoButton
-            )
-        Fractal.defineComponent render
-        |> Fractal.createComponent
-        |> Fractal.createElement props
+                DOM.footer( [| ClassName "footer" |],
+                    DOM.span( [| ClassName "todo-count" |],
+                        DOM.strong([| |], c.props.count),
+                        " todo(s) left"
+                    ),
+                    DOM.ul( [| ClassName "filters" |],
+                        DOM.li( [||],
+                            DOM.a( [| Href "#/"; ClassName (if c.props.nowShowing = FilterTodo.All then "selected" else "" ) |]  , "All"),
+                            " ",
+                            DOM.a( [| Href "#/active"; ClassName (if c.props.nowShowing = FilterTodo.Active then "selected" else "" ) |]  , "Active"),
+                            " ",
+                            DOM.a( [|Href "#/completed"; ClassName (if c.props.nowShowing = FilterTodo.Completed then "selected" else "" ) |]  , "Completed")
+                        )
+                    ),
+                    clearButton,
+                    undoButton,
+                    redoButton)
+                )|]
 
     type TodoAppProps = {model : TodoRepository}
     type TodoAppState = {nowShowing : FilterTodo; editing : Guid option; todos: Todo array }
     type todoApp = FractalComponent<TodoAppProps, TodoAppState>
 
-    let TodoApp props =
-        let getInitialState (c : todoApp) =
-            {nowShowing = FilterTodo.All; editing = None; todos = [||]}
-
-        let componentDidMount (c : todoApp) =
-            do Message.subscribe "todo.view.editDone" (fun n ->
-                c.setState({c.state with editing = None})
-            )
-
-            do Message.subscribe "todo.view.edit" (fun n ->
-                c.setState({c.state with editing = Some n})
-            )
-
-            do Message.subscribe "todo.repository.changed" (fun n ->
-                c.setState {c.state with todos = c.props.model.getState () |> List.toArray}
-            )
-
-            //TODO ROUTING
-            ()
-
+    module TodoAppHandlers =
         let handleKeyDown (c : todoApp) (e : React.KeyboardEvent) =
             if e.which = 13. then
                 e.preventDefault()
@@ -167,43 +143,55 @@ module App =
             e.target.check
             |> Message.publish "todo.repository.toggleAll"
 
-        let render (c : todoApp) =
-            let todos = c.state.todos
-            let todoItems = todos |> Array.map(fun t ->
-                let ed = if c.state.editing.IsSome then c.state.editing.Value = t.id else false
-                TodoItem {todo = t; editing = ed }  )
-            let activeCount = todos |> Array.fold (fun acc t ->
-                if t.completed then acc else acc + 1 ) 0
-            let completedCount = todos.Length - activeCount
+    let TodoApp =
+        Fractal.createComponent [|
+            GetInitialState (fun (c : todoApp) -> {nowShowing = FilterTodo.All; editing = None; todos = [||]})
 
-            let footer =
-                let props = {
-                    count = activeCount
-                    completeCount = completedCount
-                    canUndo = c.props.model.canUndo ()
-                    canRedo = c.props.model.canRedo ()
-                    nowShowing = c.state.nowShowing }
-                TodoFooter props
-
-            let main =
-                DOM.section( [| ClassName "main" |],
-                    DOM.input( [| ClassName "toggle-all"; Attr.Type "checkbox"; OnChange (toggleAll c); Checked (activeCount = 0) |]),
-                    DOM.ul( [| ClassName "todo-list" |], todoItems)
+            ComponentDidMount (fun (c : todoApp) ->
+                do Message.subscribe "todo.view.editDone" (fun n ->
+                    c.setState({c.state with editing = None})
+                )
+                do Message.subscribe "todo.view.edit" (fun n ->
+                c.setState({c.state with editing = Some n})
                 )
 
-            DOM.div( [||],
-                DOM.header( [|ClassName "header" |],
-                    DOM.h1( [||], "todos" ),
-                    DOM.input( [| Ref "newField"; ClassName "new-todo"; Placeholder "What needs to be done?"; OnKeyDown (handleKeyDown c); AutoFocus true |])
-                ),
-                (if todos.Length > 0 then main else null |> unbox<DOMElement<obj>>),
-                footer)
+                do Message.subscribe "todo.repository.changed" (fun n ->
+                    c.setState {c.state with todos = c.props.model.getState () |> List.toArray}
+                )
+            )
 
-        Fractal.defineComponent render
-        |> Fractal.getInitialState getInitialState
-        |> Fractal.componentDidMount componentDidMount
-        |> Fractal.createComponent
-        |> Fractal.createElement props
+            Render (fun (c : todoApp) ->
+                let todos = c.state.todos
+                let todoItems = todos |> Array.map(fun t ->
+                    let ed = if c.state.editing.IsSome then c.state.editing.Value = t.id else false
+                    TodoItem {todo = t; editing = ed }  )
+                let activeCount = todos |> Array.fold (fun acc t ->
+                    if t.completed then acc else acc + 1 ) 0
+                let completedCount = todos.Length - activeCount
+
+                let footer =
+                    let props = {
+                        count = activeCount
+                        completeCount = completedCount
+                        canUndo = c.props.model.canUndo ()
+                        canRedo = c.props.model.canRedo ()
+                        nowShowing = c.state.nowShowing }
+                    TodoFooter props
+
+                let main =
+                    DOM.section( [| ClassName "main" |],
+                        DOM.input( [| ClassName "toggle-all"; Attr.Type "checkbox"; OnChange (TodoAppHandlers.toggleAll c); Checked (activeCount = 0) |]),
+                        DOM.ul( [| ClassName "todo-list" |], todoItems)
+                    )
+
+                DOM.div( [||],
+                    DOM.header( [|ClassName "header" |],
+                        DOM.h1( [||], "todos" ),
+                        DOM.input( [| Ref "newField"; ClassName "new-todo"; Placeholder "What needs to be done?"; OnKeyDown (TodoAppHandlers.handleKeyDown c); AutoFocus true |])
+                    ),
+                    (if todos.Length > 0 then main else null |> unbox<DOMElement<obj>>),
+                    footer))
+        |]
 
     let app () =
         { model = TodoRepository () }
